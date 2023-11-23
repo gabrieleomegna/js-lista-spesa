ae126b98c74bbb1bafbc8fe3a8ffe26ab0a797db
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-23 19:02:52 +0100

    Implemented script, but there is an error

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
 // gli elementi della lista individualmente con un ciclo while.
 
+const listaSpesa = [
+    'uova', 
+    'zucchero', 
+    'pane', 
+    'dentifricio',
+    'farina'
+];
+
+const wrapperElement = document.querySelector ('div.wrapper');
+
+
+
+let i = 0;
+let listaSpesaItem
+while (i < listaSpesa.lenght) {
+    // const writelistaSpesaItem = document.createElement('p');
+    listaSpesaItem = listaSpesa[i];
+    console.log(listaSpesaItem);
+    // wrapperElement.appendChild(listaSpesaItem);
+    i = i + 1;
+}
